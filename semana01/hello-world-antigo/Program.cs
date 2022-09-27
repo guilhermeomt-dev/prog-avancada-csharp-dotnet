2168a8ad25bb6e17b980f6800eed7747fb694748
Guilherme Tavares <contato@guilhermeomt.dev>
2022-09-27 17:32:50 -0300

    feat: semana 01

---
+++ semana01/hello-world-antigo/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Press any key to continue . . . ");
+        Console.ReadKey(true);
+    }
+}
